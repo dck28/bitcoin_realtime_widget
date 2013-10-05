5235dea1e9f244e0e8c3a4ebee051d699929eb2e
Dennis Kong <dck28@cornell.edu>
2013-10-05 15:28:05 +0800

    widget layout version 1

---
+++ BitcoinRealtimeWidget/src/main/java/com/streetcred/bitcoinrealtimewidget/XBTRealtimeWidgetProvider.java
@@ -0,0 +1,16 @@
+package com.streetcred.bitcoinrealtimewidget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+/**
+ * Created by denniskong on 10/5/13.
+ */
+public class XBTRealtimeWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // do something
+    }
+}
