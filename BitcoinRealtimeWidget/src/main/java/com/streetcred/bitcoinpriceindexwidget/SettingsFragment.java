9a0cb2f30c332c39343a6ce59d241550620e52d1
Dennis Kong <dck28@cornell.edu>
2013-10-27 12:55:47 +0800

    SettingsFragment -added support for Chinese

--- BitcoinRealtimeWidget/src/main/java/com/streetcred/bitcoinpriceindexwidget/SettingsFragment.java
+++ BitcoinRealtimeWidget/src/main/java/com/streetcred/bitcoinpriceindexwidget/SettingsFragment.java
@@ -27,14 +27,18 @@ import java.util.concurrent.TimeUnit;
 
 public class SettingsFragment extends PreferenceFragment {
     public static final String FRAGMENT_TAG = getFragmentTag(SettingsFragment.class);
-
+    public boolean isChinese = true;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         // Load the preferences from an XML resource
-        addPreferencesFromResource(R.xml.preferences);
+        if (isChinese){
+            addPreferencesFromResource(R.xml.preferences_chinese);
+        } else {
+            addPreferencesFromResource(R.xml.preferences);
+        }
 
         getActivity().getActionBar().setDisplayHomeAsUpEnabled(false);
         getActivity().getActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
@@ -46,45 +50,25 @@ public class SettingsFragment extends PreferenceFragment {
                 ClipboardManager clipboard = (ClipboardManager) getActivity().getSystemService(Context.CLIPBOARD_SERVICE);
                 ClipData clip = ClipData.newPlainText("Bitcoin Address", "13hwfZqGQrsNXEhx1riRpFog5JPdPJBLGH");
                 clipboard.setPrimaryClip(clip);
-                Toast.makeText(getActivity(), "Address Copied. Thank You for Your Support!\n13hwfZqGQrsNXEhx1riRpFog5JPdPJBLGH", Toast.LENGTH_LONG).show();
+                if (isChinese){
+                    Toast.makeText(getActivity(), "複製成功. 很感激您的支持!\n13hwfZqGQrsNXEhx1riRpFog5JPdPJBLGH", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getActivity(), "Address Copied. Thank You for Your Support!\n13hwfZqGQrsNXEhx1riRpFog5JPdPJBLGH", Toast.LENGTH_LONG).show();
+                }
                 return true;
             }
         });
 
-        DisplayThemePreferenceDialog themePreference = (DisplayThemePreferenceDialog) findPreference("pref_theme");
-        if (themePreference != null){
-            if (themePreference.getValue() == null) {
-                themePreference.setValueIndex(0);
-            }
-            themePreference.setTitle("Theme: " + themePreference.getValue());
-            themePreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
-                @Override
-                public boolean onPreferenceChange(Preference preference,
-                                                  Object newValue) {
-                    XBTWidgetApplication.getSharedPreferences()
-                            .edit()
-                            .putString(Constants.PREF_LAST_UPDATED_THEME, newValue.toString())
-                            .commit();
-                    preference.setTitle("Theme: " + newValue.toString());
-                    try{
-                        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(getActivity());
-                        RemoteViews remoteViews = new RemoteViews(getActivity().getPackageName(), R.layout.widget_layout);
-                        ComponentName thisWidget = new ComponentName(getActivity(), XBTRealtimeWidgetProvider.class);
-                        if (newValue.toString().equalsIgnoreCase("Navy"))
-                            remoteViews.setInt(R.id.background, "setBackgroundColor",
-                                    Color.parseColor("#DD2B3856"));
-                        else if (newValue.toString().equalsIgnoreCase("Float"))
-                            remoteViews.setInt(R.id.background, "setBackgroundColor",
-                                    Color.TRANSPARENT);
-                        appWidgetManager.updateAppWidget(thisWidget, remoteViews);
-                    } catch (Exception e){
-                        e.printStackTrace();
-                    }
-                    return true;
-                }
-            });
+        if (isChinese){
+            DisplayThemePreference_Chinese();
+            DisplayCurrencyPreference_Chinese();
+        } else {
+            DisplayThemePreference_English();
+            DisplayCurrencyPreference_English();
         }
+    }
 
+    private void DisplayCurrencyPreference_English() {
         DisplayCurrencyPreferenceDialog currencyPreference = (DisplayCurrencyPreferenceDialog) findPreference("pref_currency");
         if (currencyPreference != null){
             if (currencyPreference.getValue() == null) {
@@ -114,6 +98,120 @@ public class SettingsFragment extends PreferenceFragment {
         }
     }
 
+    private void DisplayThemePreference_English() {
+        DisplayThemePreferenceDialog themePreference = (DisplayThemePreferenceDialog) findPreference("pref_theme");
+        if (themePreference != null){
+            if (themePreference.getValue() == null) {
+                themePreference.setValueIndex(0);
+            }
+            themePreference.setTitle("Theme: " + themePreference.getValue());
+            themePreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference,
+                                                  Object newValue) {
+                    XBTWidgetApplication.getSharedPreferences()
+                            .edit()
+                            .putString(Constants.PREF_LAST_UPDATED_THEME, newValue.toString())
+                            .commit();
+                    preference.setTitle("Theme: " + newValue.toString());
+                    try {
+                        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(getActivity());
+                        RemoteViews remoteViews = new RemoteViews(getActivity().getPackageName(), R.layout.widget_layout);
+                        ComponentName thisWidget = new ComponentName(getActivity(), XBTRealtimeWidgetProvider.class);
+                        if (newValue.toString().equalsIgnoreCase("Navy"))
+                            remoteViews.setInt(R.id.background, "setBackgroundColor",
+                                    Color.parseColor("#DD2B3856"));
+                        else if (newValue.toString().equalsIgnoreCase("Float"))
+                            remoteViews.setInt(R.id.background, "setBackgroundColor",
+                                    Color.TRANSPARENT);
+                        appWidgetManager.updateAppWidget(thisWidget, remoteViews);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    return true;
+                }
+            });
+        }
+    }
+
+    private void DisplayCurrencyPreference_Chinese() {
+        DisplayCurrencyPreferenceDialog currencyPreference = (DisplayCurrencyPreferenceDialog) findPreference("pref_currency_chinese");
+        if (currencyPreference != null){
+            if (currencyPreference.getValue() == null) {
+                currencyPreference.setValueIndex(0);
+            }
+            currencyPreference.setTitle("顯示貨幣: " + currencyPreference.getValue());
+            currencyPreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference,
+                                                  Object newValue_Chinese) {
+                    String newStringValue = convertCurrencyChineseToEnglish(newValue_Chinese.toString());
+                    XBTWidgetApplication.getSharedPreferences()
+                            .edit()
+                            .putString(Constants.PREF_LAST_UPDATED_CURRENCY, newStringValue)
+                            .commit();
+                    preference.setTitle("顯示貨幣: " + newValue_Chinese.toString());
+                    try{
+                        RefreshData refresh = new RefreshData();
+                        refresh.execute().get(10000, TimeUnit.MILLISECONDS);
+                    } catch (Exception e){
+                        RemoteViews remoteViews = new RemoteViews(getActivity().getPackageName(), R.layout.widget_layout);
+                        remoteViews.setTextViewText(R.id.update_time, "* 綱絡未能連接");
+                        AppWidgetManager.getInstance(getActivity()).updateAppWidget(new ComponentName(getActivity(), XBTRealtimeWidgetProvider.class), remoteViews);
+                    }
+                    return true;
+                }
+            });
+        }
+    }
+
+    private String convertCurrencyChineseToEnglish(String newValue){
+        if(newValue.equals("美元")){
+            return "USD";
+        } else if(newValue.equals("英鎊")){
+            return "GBP";
+        } else if(newValue.equals("歐元")){
+            return "EUR";
+        }
+        return "USD";
+    }
+
+    private void DisplayThemePreference_Chinese() {
+        DisplayThemePreferenceDialog themePreference = (DisplayThemePreferenceDialog) findPreference("pref_theme_chinese");
+        if (themePreference != null){
+            if (themePreference.getValue() == null) {
+                themePreference.setValueIndex(0);
+            }
+            themePreference.setTitle("顯示主題: " + themePreference.getValue());
+            themePreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference,
+                                                  Object newValue) {
+                    XBTWidgetApplication.getSharedPreferences()
+                            .edit()
+                            .putString(Constants.PREF_LAST_UPDATED_THEME, newValue.toString())
+                            .commit();
+                    preference.setTitle("顯示主題: " + newValue.toString());
+                    try{
+                        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(getActivity());
+                        RemoteViews remoteViews = new RemoteViews(getActivity().getPackageName(), R.layout.widget_layout);
+                        ComponentName thisWidget = new ComponentName(getActivity(), XBTRealtimeWidgetProvider.class);
+                        if (newValue.toString().equalsIgnoreCase("深藍色"))
+                            remoteViews.setInt(R.id.background, "setBackgroundColor",
+                                    Color.parseColor("#DD2B3856"));
+                        else if (newValue.toString().equalsIgnoreCase("漂浮"))
+                            remoteViews.setInt(R.id.background, "setBackgroundColor",
+                                    Color.TRANSPARENT);
+                        appWidgetManager.updateAppWidget(thisWidget, remoteViews);
+                    } catch (Exception e){
+                        e.printStackTrace();
+                    }
+                    return true;
+                }
+            });
+        }
+    }
+
     @Override
     public void onResume() {
         super.onResume();
@@ -136,8 +234,14 @@ public class SettingsFragment extends PreferenceFragment {
             if (rateUsPref == null) {
                 rateUsPref = new Preference(getActivity());
                 rateUsPref.setKey("prefer_rate_and_feedback");
-                rateUsPref.setTitle("Rate this widget");
-                rateUsPref.setSummary("Give feedback & feature suggestions");
+                if (isChinese){
+                    rateUsPref.setTitle("評論此應用程式");
+                    rateUsPref.setSummary("請給予您的意見及其他功能建議");
+                } else {
+                    rateUsPref.setTitle("Rate this widget");
+                    rateUsPref.setSummary("Give feedback & feature suggestions");
+                }
+
                 rateUsPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
                     @Override
                     public boolean onPreferenceClick(Preference preference) {
