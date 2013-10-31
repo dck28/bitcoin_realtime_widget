a31f8b87d0b2ba326d050aa8e98d531cdbc105c3
dck28 <dck28@cornell.edu>
2013-10-31 16:49:43 +0800

    added Font resources: preparation for font customizability

--- BitcoinRealtimeWidget/src/main/java/com/streetcred/bitcoinpriceindexwidget/FontManager.java
+++ BitcoinRealtimeWidget/src/main/java/com/streetcred/bitcoinpriceindexwidget/FontManager.java
@@ -1,7 +1,26 @@
 package com.streetcred.bitcoinpriceindexwidget;
 
+import android.content.Context;
+import android.graphics.Typeface;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by denniskong on 10/31/13.
  */
 public class FontManager {
+
+    private static Map<String, Typeface> mFonts = new HashMap<String, Typeface>();
+
+    public static Typeface getFont(Context c, String name) {
+
+        if(mFonts.containsKey(name)) {
+            return mFonts.get(name);
+        } else {
+            mFonts.put(name, Typeface.createFromAsset(c.getAssets(), name + ".ttf"));
+            return mFonts.get(name);
+        }
+    }
+
 }
